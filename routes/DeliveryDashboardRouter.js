0f065761f1380730e040cdea90a84f012a4d6332
Zorcis Calderon <zorciscalderon793@gmail.com>
2026-04-09 11:47:08 -0400

    Delivery Dashboard Admin Controller and Router

---
+++ routes/DeliveryDashboardRouter.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import { getDeliveryDashboard, postStatusDelivery } from '../controllers/DeliveryDashboardController.js';
+
+
+const router = express.Router();
+
+router.get("/delivery/DeliveryDashboardAdmin", getDeliveryDashboard);
+router.post("/delivery/DeliveryDashboardAdmin", postStatusDelivery);
